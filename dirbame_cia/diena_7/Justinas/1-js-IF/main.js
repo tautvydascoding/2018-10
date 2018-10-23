be25cc1f695f631d4c916023a2f01a82807852ed
JustinasTa <tamosaitis@gmail.com>
2018-10-23 12:31:05 +0300

    .

--- dirbame_cia/diena_7/Justinas/1-js-IF/main.js
+++ dirbame_cia/diena_7/Justinas/1-js-IF/main.js
@@ -38,11 +38,37 @@
 // jeigu vardai sutampa, i konsole pranesti apie tai
 // jeigu vardai nesutampa - parasysi "vardai yra skirtingi"
 
+let salis; //deklaracija - susikurei kintamaji
+let vardas1 = "Tomas"; // inicializacija - kintamajam priskirta pradine reiksme
+let vardas2 = "Karolis";
+
+if (vardas1 == vardas2) {
+    console.log( vardas1, vardas2 , ": abu vardai yra tokie patys ");
+}else {
+    console.log( vardas1, vardas2 , ": abu vardai yra NEVIENODI!!!");
+}
+
 // B)
 // Jeigu vardas1 yra "Tomas" - pasisveikinti su juo
-
+if (vardas1 == "Tomas") {
+  console.log(vardas1, "Labas Tomai");
+}else {
+  console.log(vardas1, "Tu ne Tomas" );
+}
 // C)
 // Patikrinti ar varda1 yra "Tomas", IR butinai vardas2  yra "Karolis"
+
+if (vardas1 == "Tomas" && vardas2 == "Karolis") {
+  console.log("Vardai yra ", vardas1, "ir", vardas2);
+}else {
+  console.log("Cia yra", vardas1, "ir Karolis");
+}
+
+if (vardas1 == "Tomas" || vardas2 =="Karolis") {
+  console.log("vienas ir vardu yra Tomas", vardas1, vardas2);
+}else {
+  console.log("tarp vardu nera Tomo", vardas1, vardas2);
+}
 
 // D)
 // Patikrinti ar bent vienas is vardu "Tomas"
@@ -51,12 +77,22 @@
 
 // 1 UZDUOTIS
 // turesime vartotojo amziu
-// var age = 24;
-// gapal ji, turesiem isvesti tam tikra reklamos teksta
-
-
+let age = 66;
+// pagal ji, turesiem isvesti tam tikra reklamos teksta
+if ((age < 7) || (age > 65)) {
+console.log("Pliusines varles", "ir", "Sokoladiniai zuikuciai 20% nuolaida");
+} else if (age < 14) {
+console.log("Mini telefonai");
+} else if (age < 18) {
+console.log("new Music App");
+} else if (age < 24) {
+console.log("Stok i sauliu sajunga");
+} else if (age <= 65) {
+  console.log("Pensijos kaupimas - uzsiregistruok");
+} else if (age > 65) {
+  console.log("kelione i Bristona", "ir", "Sokoladiniai zuikuciai 20% nuolaida");
+}
 // Salygos:
-
 // iki 7 metu
 //      "Pliusines varles"
 // nuo 7 iki 14
