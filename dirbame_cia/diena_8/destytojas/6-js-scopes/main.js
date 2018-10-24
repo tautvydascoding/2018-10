79096e700a5a60242b1ee471e91c34ea777eb382
Tautvydas <tautvydascoding@gmail.com>
2018-10-24 16:32:07 +0300

    darbai klases

---
+++ dirbame_cia/diena_8/destytojas/6-js-scopes/main.js
@@ -0,0 +1,19 @@
+// ====================Scope====================
+//   UZDUOTIS  A
+// sukurti funkcija "printDuomenys()" ,
+// funkcijoje sukurti kintamaji: var vardas = "Tomas"
+// kuri  atspausdina i konsole  kintamaji "vardas"
+// iskviesti f-ja
+
+
+function printDuomenys() {
+    var vardas = "Tomas";
+    console.log( vardas ); // Tomas
+}
+printDuomenys();
+console.log( vardas ); // error
+
+//   UZDUOTIS  B
+//  dabar, pabandykite po f-ja (uz jos ribu) i konsole atspausdinti kintamaji "vardas"
+//  console.log( vardas) ;
+// NOTE: gaunate kalida ar ne?console.log(   "Labas"  );
